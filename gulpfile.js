998b9d57c120c066a852d1b00d3b6d19208f5164
JohnMagtoto <johnmagtoto@gmail.com>
2016-05-26 18:37:18 +0800

    end-of-day commit: working on gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp');
+const del = require('del');
+const typescript = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
+const runsequence = require('run-sequence') ;
+
+const tscConfig = require('./tsconfig.json');
+
+// clean the contents of the distribution directory
+gulp.task('clean', function () {
+  return del('dist/**/*');
+});
+
+// TypeScript compile
+gulp.task('compile', ['clean'], function () {
+  return gulp
+    .src('src/client/app/**/*.ts')
+    .pipe(typescript(tscConfig.compilerOptions))
+    .pipe(sourcemaps.init())          // <--- sourcemaps
+    .pipe(typescript(tscConfig.compilerOptions))
+    .pipe(sourcemaps.write('.'))      // <--- sourcemaps
+    .pipe(gulp.dest('dist/app'));
+});
+
+// copy dependencies
+gulp.task('copy:vendor', ['clean', 'copy:angular'], function() {
+  return gulp.src([
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/systemjs/dist/system.src.js',
+      'node_modules/rxjs/bundles/Rx.js',
+      'node_modules/core-js/client/shim.min.js',
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/reflect-metadata/Reflect.js'
+    ])
+    .pipe(gulp.dest('dist/vendor'));
+});
+
+// copy Angular 2 
+gulp.task('copy:angular', ['clean'], function(){
+  return gulp.src([
+    'node_modules/@angular/**/*'
+  ])
+  .pipe(gulp.dest('dist/vendor/@angular'))
+}) ;
+
+// copy static assets - i.e. non TypeScript compiled source
+gulp.task('copy:assets', ['clean'], function() {
+  return gulp.src(['src/client/**/*', 'index.html', 'styles.css', '!**/*.ts'], { base : './src/client' })
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('build', ['compile', 'copy:vendor', 'copy:assets']);
+gulp.task('default', ['build']);
